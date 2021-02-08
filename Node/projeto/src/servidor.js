3cf822370ec5ae8951506b1468b286ad9af9759f
wesleyMirand <wewesantos@hotmail.com>
2021-02-07 22:05:33 -0200

    ´changes´

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -2,11 +2,24 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancodedados')
 
 app.get('/produtos', (req, res, next ) => {
-  res.send({ nome:'Notebook', preco: 123.45})
+  res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+  res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    nome: req.body.name,
+    preco: req.body.preco
+  })
+  res.send(produto) //JSON
 })
 
 app.listen(porta, () => {
-  console.log(`Servidor executando na porta ${porta}.`)
+  console.log(`Servidor esta executando na porta ${porta}.`)
 })
