0d26dbd3017529fc140fcadde392c80bec9a8e9b
wesleyMirand <wewesantos@hotmail.com>
2021-04-24 01:02:40 -0300

    ´changes´

--- ajax/server.js
+++ ajax/server.js
@@ -6,5 +6,4 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Executando...'))
