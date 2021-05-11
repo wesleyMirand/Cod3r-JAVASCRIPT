fc32467f2a2cc6c8924fbe26f2abde166f47967e
wesleyMirand <wewesantos@hotmail.com>
2021-05-10 23:58:06 -0300

    ´changes´

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,9 +1,23 @@
 const gulp = require('gulp')
 const series  = gulp.series
 
+const antes1 = cb => {
+  console.log('Tarefa Antes 1!')
+  return cb()
+}
+
+const antes2 = cb => {
+  console.log('Tarefa Antes 2!')
+  return cb()
+}
+
 function copiar (cb) {
   console.log('Tarefa de copiar')
   return cb()
 }
 
+const fim = cb => {
+  console.log('Tarefa Fim!')
+  return cb()
+}
 module.exports.default = series(copiar)
