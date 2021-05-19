e651690265940ebf6274be280b10b2b969e0cb39
wesleyMirand <wewesantos@hotmail.com>
2021-05-18 23:59:04 -0300

    ´chages´

--- gulp/css/gulpfile.js
+++ gulp/css/gulpfile.js
@@ -5,11 +5,11 @@ const uglifycss = require ('gulp-uglifycss')
 const concat = require ('gulp-concat')
 
 function transformacaoCSS() {
-  return gulp.src('src/sass/index.css')
+  return gulp.src('src/sass/index.scss')
       .pipe(sass().on('error', sass.logError))
-      .pipe(uglifycss({ "uglyComments":true}))
+      .pipe(uglifycss({ "uglyComments": true}))
       .pipe(concat("estilo.min.css"))
       .pipe(gulp.dest('build/css'))
 }
-
+ 
 exports.default = series(transformacaoCSS)
