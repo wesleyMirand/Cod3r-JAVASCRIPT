5ba88b7f31673bc52f58f5275431e662863e173a
wesleyMirand <wewesantos@hotmail.com>
2021-05-19 23:57:38 -0300

    ´chages´

--- gulp/css/gulpfile.js
+++ gulp/css/gulpfile.js
@@ -11,5 +11,10 @@ function transformacaoCSS() {
       .pipe(concat("estilo.min.css"))
       .pipe(gulp.dest('build/css'))
 }
+
+function copiarHTML() {
+  return gulp.src('src/index.html')
+      .pipe(gulp.dest('build'))
+}
  
 exports.default = series(transformacaoCSS)
