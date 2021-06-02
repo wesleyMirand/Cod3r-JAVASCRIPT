524f4403986e59f966980d178b3fc8f66e9bacaa
wesleyMirand <wewesantos@hotmail.com>
2021-06-01 23:57:38 -0300

    ´chages´

--- gulp/spa/gulpTasks/app.js
+++ gulp/spa/gulpTasks/app.js
@@ -1,11 +1,23 @@
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 
 function appHtml (cb) {
-  return cb ()
+  return gulp.src ('src/**/*.html')
+      .pipe(htmlmin({ collapseWhitespace : true}))
+      .pipe(gulp.dest('build'))
 }
 
-function appCSS(cb) {
-  return cb ()
+function appCSS() {
+  return gulp.src(src/assets/sass/index.scss)
+      .pipe(sass().on('error', sass.logError))
+      .pipe(uglifycss({ "uglyComments": true}))
+      .pipe(concat('app.min.css'))
+      .pipe(gulp.dest('build/assets/css'))
 }
 
 function appJs (cb) {
