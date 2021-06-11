0c4ee0d8700f8c49ba766ae6864f2cb53af81b41
wesleyMirand <wewesantos@hotmail.com>
2021-06-10 23:05:40 -0300

    ´chages´

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,4 +1,5 @@
 import Pessoa from './pessoa'
+import './assets/css/estilo.css'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,19 @@
-const webpack = require ('webpack')
+const webpack = require('webpack')
 
 module.exports = {
   mode: 'development',
-  entry: './src/principal.js'
+  entry: './src/principal.js',
+  output: {
+    filename: 'principal.js',
+    path: __dirname + '/public'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader', //adicina o CSS a DOM injetando a tag <style>
+        'css-loader' //interpreta @import, url()...
+      ]
+    }]
+  }
 }
