2395a31a1b45a602041a89231307b838978f79f1
wesleyMirand <wewesantos@hotmail.com>
2021-06-11 23:55:57 -0300

    ´chages´

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require ('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
@@ -7,11 +8,17 @@ module.exports = {
     filename: 'principal.js',
     path: __dirname + '/public'
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename:"estilo.css"
+    })
+  ],
   module: {
     rules: [{
       test: /\.css$/,
       use: [
-        'style-loader', //adicina o CSS a DOM injetando a tag <style>
+        MiniCssExtractPlugin.loader,
+        //'style-loader', //adicina o CSS a DOM injetando a tag <style>
         'css-loader' //interpreta @import, url()...
       ]
     }]
