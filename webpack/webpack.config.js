5de59e8f3667cbdac57e2407852a799b763db912
wesleyMirand <wewesantos@hotmail.com>
2021-06-15 22:42:49 -0300

    ´chages´

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,17 +1,33 @@
+const modoDev=process.env.NODE_ENV != 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require ('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require ('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
+  mode: modoDev ? 'development' : 'production',
   entry: './src/principal.js',
   output: {
     filename: 'principal.js',
     path: __dirname + '/public'
   },
+  devServer: {
+    contentBase: "./public",
+    port: 9000
+
+  },
+  optimization : {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename:"estilo.css"
-    })
+    new TerserPlugin({
+      parallel: true,
+      terserOptions: {
+        ecma: 6,
+      },
+    }),
   ],
   module: {
     rules: [{
@@ -22,6 +38,9 @@ module.exports = {
         'css-loader', //interpreta @import, url()...
         'sass-loader',
       ]
+    }, {
+      test:/\.(png| svg | jpg | gif)$/,
+      use: [ 'file-loader']
     }]
   }
 }
