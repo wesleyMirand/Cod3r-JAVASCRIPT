0e2617b81d5ceb134ff676476cc9c4f6ef8209c0
wesleyMirand <wewesantos@hotmail.com>
2021-07-13 19:55:58 -0300

    ´chages´

--- bootstrap/projeto-galeria/src/js/core/includes.js
+++ bootstrap/projeto-galeria/src/js/core/includes.js
@@ -3,7 +3,7 @@ import $ from 'jquery'
 function loadIncludes(parent) {
   if(!parent) parent = 'body'
   $(parent).fund('[wm-include]').each(function(i, e){
-    cont url = $(e),attr('wm-include')
+    const url = $(e),attr('wm-include')
     $.ajax({
       url,
       success(data) {
