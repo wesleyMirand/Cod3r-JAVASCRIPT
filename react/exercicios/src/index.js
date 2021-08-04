4aaaff3824a4d0617b1cc2e7a6f48a84a7b8a4f1
wesleyMirand <wewesantos@hotmail.com>
2021-08-03 22:22:55 -0300

    ´chages´

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const elemento = <h1>React 2</h1>
-ReactDOM.render(elemento, document.getElementById('root'))
+import Primeiro from './componentes/Primeiro'
+
+ReactDOM.render(<Primeiro/>, document.getElementById('root'))
