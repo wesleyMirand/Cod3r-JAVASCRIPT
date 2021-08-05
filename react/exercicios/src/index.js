dccc28f541d67e29b43b626020f951a28f739fbd
wesleyMirand <wewesantos@hotmail.com>
2021-08-04 22:45:34 -0300

    ´chages´

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
 
-ReactDOM.render(<Primeiro/>, document.getElementById('root'))
+
+ReactDOM.render(<BomDia nome="Wesley"/>, document.getElementById('root'))
