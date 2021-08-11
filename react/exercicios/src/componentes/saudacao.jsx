9ada5eaad0c879921f4803695d640e1b4174a1bd
wesleyMirand <wewesantos@hotmail.com>
2021-08-10 23:31:31 -0300

    ´chages´

--- react/exercicios/src/componentes/saudacao.jsx
+++ react/exercicios/src/componentes/saudacao.jsx
@@ -7,6 +7,11 @@ export default class Saudacao extends Component {
     nome: this.props.nome
   }
 
+  constructor (props) {
+    super(props)
+    this.setTipo = this.setTipo.bind(this) 
+  }
+
   setTipo (e) {
     this.setState({ tipo: e.target.value})
   }
@@ -22,9 +27,9 @@ export default class Saudacao extends Component {
         <h1> {tipo} {nome}!</h1>
         <hr />
         <input type = "text" placeholder="Tipo..." 
-        value = {tipo} onChange= {e => this.setTipo(e)} />
+        value = {tipo} onChange= {this.setTipo} />
         <input type = "text" placeholder="Nome..." 
-        value = {nome} onChange= {e => this.setNome(e)}/>
+        value = {nome} onChange= { e =>this.setNome (e)} />
       </div>
     )
   }
