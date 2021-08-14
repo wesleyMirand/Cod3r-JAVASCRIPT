dd9cbf751e9678f42a1fd0f1f8ad864696f5b647
wesleyMirand <wewesantos@hotmail.com>
2021-08-13 22:56:22 -0300

    ´chages´

--- react/calculadora/src/index.js
+++ react/calculadora/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Calculator from './main/Calculator';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+, document.getElementById('root'));
+registerServiceWorker();
