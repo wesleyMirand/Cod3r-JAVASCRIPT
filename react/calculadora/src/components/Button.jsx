d33ef45d2928fd817fb38bcd87431090ccdd0d2a
wesleyMirand <wewesantos@hotmail.com>
2021-08-21 21:44:50 -0300

    ´chages´

--- react/calculadora/src/components/Button.jsx
+++ react/calculadora/src/components/Button.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Button.css'
 
-export default props =>
-<button className = {`
-    
-`}>
-      {props.label}
-</button>
+export default props => {
+  let classes = 'button '
+  classes += props.operation ? 'operation' : ''
+  classes += props.double ? 'operation' : ''
+  classes += props.triple ? 'operation' : ''
+
+  return (
+  <button 
+    onClick = { e => props.click (props.label)}
+    className={classes}>
+    {props.label}
+  </button>
+  )
+}
