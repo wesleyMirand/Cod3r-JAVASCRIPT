da27f08121918f2ad139aa74ba000847251546aa
wesleyMirand <wewesantos@hotmail.com>
2021-09-15 23:57:43 -0300

    ´chages´

--- react/crud/frontend/src/componentes/template/Logo.jsx
+++ react/crud/frontend/src/componentes/template/Logo.jsx
@@ -1,7 +1,10 @@
 import './Logo.css'
+import logo from '../../assests/imgs/logo.png'
 import React from 'react'
 
 export default props =>
     <aside className= "logo" >
-      Logo
+      <a href="/" className="logo">
+        <img src= {logo} alt="logo" />
+      </a>
     </aside>
