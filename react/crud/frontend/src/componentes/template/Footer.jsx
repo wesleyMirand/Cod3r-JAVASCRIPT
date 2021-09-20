ae4db00b29fab52ffa0b1abd61e2d3af9bba1712
wesleyMirand <wewesantos@hotmail.com>
2021-09-19 23:49:36 -0300

    ´chages´

--- react/crud/frontend/src/componentes/template/Footer.jsx
+++ react/crud/frontend/src/componentes/template/Footer.jsx
@@ -3,5 +3,6 @@ import React from 'react'
 
 export default props =>
   <footer className = "footer">
-    Rodapé
+    Desenvolvido com < i className= "fa fa-heart text-danger" ></i> por
+    <strong> Cod < span className="text-danger">3</span> r</strong>
   </footer>
