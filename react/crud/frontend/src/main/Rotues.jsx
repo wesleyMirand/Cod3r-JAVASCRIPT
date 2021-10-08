27982122f922e8fae1568cbf1b1c1624d2682751
wesleyMirand <wewesantos@hotmail.com>
2021-10-07 23:55:01 -0300

    ´chages´

---
+++ react/crud/frontend/src/main/Rotues.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router'
+
+import Home from '../componentes/home/home'
+import userCrud from '../componentes/user/UserCrud'
+
+export default props =>
+   <Switch>
+     
+   </Switch>
