da8a0276992969723bcb7e86252a20d61e53da70
wesleyMirand <wewesantos@hotmail.com>
2021-10-15 00:59:44 -0300

    ´chages´

--- react/crud/frontend/src/componentes/user/UserCrud.jsx
+++ react/crud/frontend/src/componentes/user/UserCrud.jsx
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component, useReducer} from 'react'
+import axios from 'axios'
 import Main from '../template/Main'
 
 const headerProps = {
@@ -7,7 +8,37 @@ const headerProps = {
   subtitle: 'Cadastro de usuários: Incluir , Listar, Alterar e Excluir'
 }
 
+const baseUrl='http://localhost:3001/users'
+const initialState = {
+  user: {name:'', email:''},
+  list:[]
+}
+
 export default class UserCrud extends Component {
+
+  state= {...initialState}
+
+  clear () {
+    this.setState({ user: initialState.user })
+  }
+
+  save () {
+    const user = this.state.user
+    const method = user.id ? 'put' : 'post'
+    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+    axios[method] (url, user)
+       .then (resp => {
+         const list = this.getUpdatedList(resp.data)
+         this.setState({user: initialState.user, list})
+       })
+  }
+
+  getUpdatedList(user) {
+    const list = this.state.list.filter (u => u.id !== user.id)
+    list.unshift(user)
+    return list
+  }
+
   render () {
     return(
     <Main {...headerProps} >
