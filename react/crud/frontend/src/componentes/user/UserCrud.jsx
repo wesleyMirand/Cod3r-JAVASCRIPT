f88eeea36a175493f76ad3d3892b0d671deec225
wesleyMirand <wewesantos@hotmail.com>
2021-11-23 00:26:42 -0200

    changes

--- react/crud/frontend/src/componentes/user/UserCrud.jsx
+++ react/crud/frontend/src/componentes/user/UserCrud.jsx
@@ -105,11 +105,51 @@ export default class UserCrud extends Component {
 
   remove (user) {
     axios.delete(`${baseUrl}/${user.id}`).then(resp => {
-      const list = this.getUpdatedList
+      const list = this.getUpdatedList(null)
       this.setState({list})
       
     })
   }
+
+  renderTable() {
+    return (
+      <table className="table mt-4">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderRows()}
+        </tbody>
+      </table>
+    )
+  }
+
+  renderRows() {
+    return this.state.list.map( user =>{
+      return(
+        <tr key={user.id}>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>
+            <button className="btn btn-warning"
+              onClick = {(fa fa-pencil)}
+              <i className="fa fa-pencil"></i>
+            </button>
+            <button className="btn btn-danger ml-2">
+              <i className="fa fa-trash">
+
+              </i>
+            </button>
+          </td>
+        </tr>
+      )
+    })
+  }
+
   render () {
     return(
     <Main {...headerProps} >
