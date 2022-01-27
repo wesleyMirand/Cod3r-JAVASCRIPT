0aa550f60ebed1e7e4d11eda12b304f4fa8e9804
wesleyMirand <wewesantos@hotmail.com>
2022-01-26 23:57:56 -0200

    primeiro commit

--- react/crud/frontend/src/componentes/template/Footer.jsx
+++ react/crud/frontend/src/componentes/template/Footer.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default props =>
   <footer className = "footer">
-    Desenvolvido com <i className= "fa fa-heart text-danger" > </i> por
+    Desenvolvido com js <i className= ""> </i> <i className= "fa fa-heart text-danger" > </i> por
     <strong> Cod <span className="text-danger"> 3 </span> r </strong>
   </footer>
